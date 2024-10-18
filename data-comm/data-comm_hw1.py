114be5a94bc0e765db48221349b56898018fa7cb
Yujin <yj999900@gmail.com>
2024-10-18 17:22:15 +0900

    .

--- data-comm/data-comm_hw1.py
+++ data-comm/data-comm_hw1.py
@@ -1,44 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 비트가 1이면 High에서 Low로 전환, 0이면 Low에서 High로 전환
+# High에서 Low로 전환되면 0, Low에서 High로 전환되면 1
 def manchester_encoding(data):
-    signal = []
+    encoded_signal = [1]
     for bit in data:
         if bit == 1:
-            signal.extend([1, 0])
+            encoded_signal.extend([0, 1])
         else:
-            signal.extend([0, 1])
-    return signal
+            encoded_signal.extend([1, 0])
+    print("Manchester: " + str(encoded_signal))
+    return encoded_signal
 
-# 비트가 1일 경우 신호 전환 X, 0일 경우 전환
+# 비트의 시작에서 신호 전환이 발생하면 0, 신호 전환이 발생하지 않으면 1
 def differential_manchester_encoding(data):
-    signal = []
-    current_signal = 1
-    for bit in data:
+    encoded_signal = [0, 1]
+    current_signal = encoded_signal[-1]
+    for bit in data[1:]:
         if bit == 1:
-            signal.extend([current_signal, current_signal ^ 1])
-        else:
-            current_signal ^= 1  # 신호 전환
-            signal.extend([current_signal, current_signal ^ 1])
-    return signal
-
+            encoded_signal.extend([current_signal, current_signal ^ 1])
+        elif bit == 0:
+            current_signal ^= 1
+            encoded_signal.extend([current_signal, current_signal ^ 1])
+    print("Differential Manchester: " + str(encoded_signal))
+    return encoded_signal
+# [0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, ...]
 # 입력 데이터
-input_data = [1, 0, 1, 1, 0, 0, 1]
+input_data = [0, 1, 0, 0, 1, 1, 0, 1, 0, 0]
 
 # 인코딩 신호 생성
 manchester_signal = manchester_encoding(input_data)
 differential_manchester_signal = differential_manchester_encoding(input_data)
-
-# 시간 축 생성
-time = np.arange(0, len(manchester_signal))
 
 # 그래프 출력
 plt.figure(figsize=(12, 6))
 
 # Manchester
 plt.subplot(2, 1, 1)
-plt.step(time, manchester_signal, where='mid')
+plt.step(np.arange(0, len(manchester_signal)), manchester_signal, where='pre')
 plt.ylim([-0.5, 1.5])
 plt.title('Manchester Encoding')
 plt.xlabel('Time')
@@ -47,7 +46,7 @@ plt.grid(True)
 
 # Differential Manchester
 plt.subplot(2, 1, 2)
-plt.step(time, differential_manchester_signal, where='mid')
+plt.step(np.arange(0, len(differential_manchester_signal)), differential_manchester_signal, where='post')
 plt.ylim([-0.5, 1.5])
 plt.title('Differential Manchester Encoding')
 plt.xlabel('Time')
