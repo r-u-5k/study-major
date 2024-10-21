cae974f7d50bd4673f184ad9953011ef7f2ea4fc
Yujin <yj999900@gmail.com>
2024-10-21 15:08:36 +0900

    .

--- data-comm/data-comm_hw1.py
+++ data-comm/data-comm_hw1.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # High에서 Low로 전환되면 0, Low에서 High로 전환되면 1
 def manchester_encoding(data):
     encoded_signal = [1]
@@ -12,29 +13,27 @@ def manchester_encoding(data):
     print("Manchester: " + str(encoded_signal))
     return encoded_signal
 
+
 # 비트의 시작에서 신호 전환이 발생하면 0, 신호 전환이 발생하지 않으면 1
 def differential_manchester_encoding(data):
-    encoded_signal = [0, 1]
+    encoded_signal = [1, 0]
     for bit in data[1:]:
         if bit == 1:
             encoded_signal.extend([encoded_signal[-1], encoded_signal[-1] ^ 1])
         elif bit == 0:
             encoded_signal.extend([encoded_signal[-1] ^ 1, encoded_signal[-1]])
     print("Differential Manchester: " + str(encoded_signal))
-    # [0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, ...]
     return encoded_signal
 
-# 입력 데이터
+
 input_data = [0, 1, 0, 0, 1, 1, 0, 1, 0, 0]
 
-# 인코딩 신호 생성
 manchester_signal = manchester_encoding(input_data)
 differential_manchester_signal = differential_manchester_encoding(input_data)
 
-# 그래프 출력
 plt.figure(figsize=(12, 6))
 
-# Manchester
+# Manchester Encoding
 plt.subplot(2, 1, 1)
 plt.step(np.arange(0, len(manchester_signal)), manchester_signal, where='pre')
 plt.ylim([-0.5, 1.5])
@@ -43,7 +42,7 @@ plt.xlabel('Time')
 plt.ylabel('Signal')
 plt.grid(True)
 
-# Differential Manchester
+# Differential Manchester Encoding
 plt.subplot(2, 1, 2)
 plt.step(np.arange(0, len(differential_manchester_signal)), differential_manchester_signal, where='post')
 plt.ylim([-0.5, 1.5])
