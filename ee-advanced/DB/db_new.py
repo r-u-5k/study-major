42bee3ea5a00339618c35de4e42c64bcdc8f6503
Yujin <yj999900@gmail.com>
2024-11-08 18:22:12 +0900

    .

--- ee-advanced/DB/db_new.py
+++ ee-advanced/DB/db_new.py
@@ -13,19 +13,19 @@ def DirectUpload(FullPath, Table, Data, Columns):
     conn = psycopg2.connect(host=pa.host, dbname=pa.dbname, user=pa.user, password=pa.password, port=pa.port)
     cur = conn.cursor()
 
-    drop_query = f"DROP TABLE IF EXISTS {Table};"
-    cur.execute(drop_query)
-
-    create_query = f"""
-    CREATE TABLE {Table} (
-        basetime TIMESTAMP,
-        fcsttime TIMESTAMP,
-        lon DOUBLE PRECISION,
-        lat DOUBLE PRECISION,
-        temp DOUBLE PRECISION
-    );
-    """
-    cur.execute(create_query)
+    # drop_query = f"DROP TABLE IF EXISTS {Table};"
+    # cur.execute(drop_query)
+    #
+    # create_query = f"""
+    # CREATE TABLE {Table} (
+    #     basetime TIMESTAMP,
+    #     fcsttime TIMESTAMP,
+    #     lon DOUBLE PRECISION,
+    #     lat DOUBLE PRECISION,
+    #     temp DOUBLE PRECISION
+    # );
+    # """
+    # cur.execute(create_query)
 
     Data.to_csv(FullPath, index=False, header=False)
     f = open(FullPath, 'r')
@@ -42,20 +42,20 @@ def DirectUpload(FullPath, Table, Data, Columns):
     return []
 
 
-def NullTesting(DBNAME, Content, TargetA, TargetB):
-    query = f"select EXISTS(select * from {DBNAME} where {Content} >= TIMESTAMP '%s' " \
-            f"AND {Content} <= TIMESTAMP '%s' " \
-            f"AND not ({DBNAME} is not null))" % (TargetA, TargetB)
+def NullTesting(Table, Content, TargetA, TargetB):
+    query = f"select EXISTS(select * from {Table} where {Content} >= TIMESTAMP '{TargetA}' " \
+            f"AND {Content} <= TIMESTAMP '{TargetB}' " \
+            f"AND not ({Table} is not null))"
     return query
 
 
-def Deleting(DBNAME, Content, TargetA):
-    query = f"delete from {DBNAME} where {Content} = {TargetA}"
+def Deleting(Table, Content, TargetA):
+    query = f"delete from {Table} where {Content} = {TargetA}"
     return query
 
 
-def DataDownloader(DB, Begin, End):
-    query = f"""select * from {DB} WHERE "DeliveryDT" >= TIMESTAMP '{Begin}' AND "DeliveryDT" <= TIMESTAMP '{End}' """
+def DataDownloader(Table, Begin, End):
+    query = f"""select * from {Table} WHERE "DeliveryDT" >= TIMESTAMP '{Begin}' AND "DeliveryDT" <= TIMESTAMP '{End}' """
     alchemyEngine = create_engine(conn_string)
     dbConnection = alchemyEngine.connect()
     Data = pd.read_sql(query, con=dbConnection)
