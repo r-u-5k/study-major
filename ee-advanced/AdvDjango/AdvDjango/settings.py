fee41575eedd1964dff0ffd4c6f4120d365ba794
Yujin <yj999900@gmail.com>
2024-11-21 17:21:25 +0900

    .

--- ee-advanced/AdvDjango/AdvDjango/settings.py
+++ ee-advanced/AdvDjango/AdvDjango/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "AdvDjango.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "yj9928",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
