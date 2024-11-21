95b85755d4929076380a400f689726ce11ef33b1
Yujin <yj999900@gmail.com>
2024-11-21 17:21:55 +0900

    Revert "."
    This reverts commit fee41575eedd1964dff0ffd4c6f4120d365ba794.

--- ee-advanced/AdvDjango/AdvDjango/settings.py
+++ ee-advanced/AdvDjango/AdvDjango/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = "AdvDjango.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "yj9928",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
